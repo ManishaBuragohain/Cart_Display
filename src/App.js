9a5edffa92ea9c033010b3cef63f4c6ed0d66883
Manisha <flowerdark777@gmail.com>
2024-02-27 23:17:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CardItems from "./CardItems";
+
+function App() {
+  return (
+    <div className="App">
+      <CardItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CardItems.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./CardItems.css";
+const CardItems = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const fetchedData = await response.json();
+      setData(fetchedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      {data.map((item) => (
+        <div key={item.id} className="card">
+          <h2>{item.title}</h2>
+          <img
+            src={item.image}
+            alt={` ${item.image}`}
+            style={{ width: "10rem", marginTop: "20px" }}
+          />
+          <p>{item.description}</p>
+          <p>${item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardItems;
